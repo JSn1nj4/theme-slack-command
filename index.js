59f98adb325d13505cc0f179bc0632bac29a0f51
Elliot Derhay <e.j.derhay@gmail.com>
2018-10-25 12:50:48 -0400

    Add first iteration of index.js
    Setup express, default port, public path, and first routes.

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000
+
+let app = express();
+
+app.use(express.static(path.join(__dirname, 'public')))
+  .use(express.json())
+  .set('json spaces', 2);
+
+app.get('/', (req, res) => res.redirect('http://elliotderhay.com'));
+
+app.get('/slack/theme', (req, res) => res.json({
+  hello: 'world'
+}));
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
